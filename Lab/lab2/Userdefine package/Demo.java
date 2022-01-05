2af32525287c16aeefdcd91d2635c8bbfe26072b
Safal Bhandari <be2019se665@gces.edu.np>
2022-01-05 20:06:40 +0545

    lab-2

---
+++ Lab/lab2/Userdefine package/Demo.java
@@ -0,0 +1,8 @@
+import com.gces.safal.PackageTry;
+public class Demo {
+    public static void main(String[] args) {
+        PackageTry demo = new PackageTry();
+        demo.showMessage();
+    }
+    
+}
---
+++ Lab/lab2/Userdefine package/PackageTry.java
@@ -0,0 +1,9 @@
+package com.gces.safal;
+public class PackageTry {
+    public void showMessage(){
+        System.out.println("Package");
+    }
+    public static void main(String[] args) {
+        System.out.println("uccess");
+    }
+}
